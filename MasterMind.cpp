1f69cbbc502c9506a3c82f9545b63eb8dd9a1849
TiagoVittorio <156953427+TiagoVittorio@users.noreply.github.com>
2024-12-23 22:22:52 +0000

    MasterMind.cpp

--- MasterMind.cpp
+++ MasterMind.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "Portuguese");
-	int numeros[4], escolha[4], postitvo = 0, quantidade = 0;
+	int numeros[4], escolha[4], postitvo = 0, quantidade = 0, i;
 	srand(time(0));
 
 	for (int i = 0; i < 4; i++)
@@ -15,13 +15,14 @@ void main()
 		i + 1;
 	}
 	do {
-		postitvo= 0;
+		postitvo = 0;
 		cout << "Tenta adivinhar os 4 números!!! ( 0/1 ): \n";
-		for (int i = 0; i < 4; i++)
+		for (i = 0; i < 4; i++)
 		{
-			cin >> escolha[i] << i + 1;
+			cin >> escolha[i];
+				i + 1;
 		}
-		for (int i = 0; i < 4; i++)
+		for (i = 0; i < 4; i++)
 		{
 			if (escolha[i] == numeros[i])
 			{
@@ -29,10 +30,10 @@ void main()
 			}
 		}
 		quantidade++;
-		cout << " Acertaste!! " << postitvo<< "\n";
+		cout << " Acertaste!! " << postitvo << "\n";
 		if (postitvo == 4)
 		{
-			cout << "Parabéns ganhaste 1 MILHAO de dolares, pensava que iria ser mais dificil \n" << "Em " << quantidade << " tentativas";
+			cout << "Parabéns ganhaste 1 milhão de dolares, pensava que iria ser mais dificil!!! \n" << "Em " << quantidade << " tentativas";
 		}
 	} while (postitvo < 4);
 
